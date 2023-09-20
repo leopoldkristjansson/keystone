a6fe6cdfaa5ce85796ddbabda5d3c768a32e0e15
Daniel Cousens <413395+dcousens@users.noreply.github.com>
2023-09-20 17:22:36 +1000

    fix segmentation fault with jest

--- packages/core/src/lib/assets/createImagesContext.ts
+++ packages/core/src/lib/assets/createImagesContext.ts
@@ -1,5 +1,7 @@
 import { v4 as uuid } from 'uuid';
+import { fileTypeFromBuffer } from 'file-type';
 import imageSize from 'image-size';
+
 import type { KeystoneConfig, ImagesContext } from '../../types';
 import type { ImageAdapter } from './types';
 import { localImageAssetsAPI } from './local';
@@ -7,7 +9,7 @@ import { s3ImageAssetsAPI } from './s3';
 import { streamToBuffer } from './utils';
 
 async function getImageMetadataFromBuffer(buffer: Buffer) {
-  const fileType = await (await import('file-type')).fileTypeFromBuffer(buffer);
+  const fileType = await fileTypeFromBuffer(buffer);
   if (!fileType) {
     throw new Error('File type not found');
   }
